407b7c943010b2a821db02145f3ed705cc5a9ea6
Stephen Zhang <zstephenj@gmail.com>
2018-09-18 23:08:07 -0500

    Created main.py, setup jinja, created index.html, welcome.html

---
+++ main.py
@@ -0,0 +1,7 @@
+from flask import Flask, request
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
